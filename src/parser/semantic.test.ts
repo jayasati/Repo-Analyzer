d6f563f5bdfd4b8542b7fc757ba6434421eb4298
jayasati <jayrasati1819@gmail.com>
2026-03-12 15:39:04 +0530

    feat(parser): implement multi-language semantic analyzer
    * added semantic entity types (classes, functions, imports)
    * implemented TreeSitterSemanticService for AST semantic extraction
    * created semantic test to validate entity detection
    * enables architecture analysis across Tree-Sitter supported languages
    
    This introduces a language-agnostic semantic layer for the analyzer.

---
+++ src/parser/semantic.test.ts
@@ -0,0 +1,27 @@
+import { TreeSitterParserService } from "./tree-sitter-parser.service";
+import { TreeSitterSemanticService } from "./tree-sitter-semantic.service";
+
+const parser = new TreeSitterParserService();
+const semantic = new TreeSitterSemanticService();
+
+parser.setLanguage("typescript");
+
+const code = `
+import { UserService } from "./user.service";
+
+class UserController {
+
+  constructor(private service: UserService) {}
+
+  getUsers() {}
+
+}
+
+function helper() {}
+`;
+
+const tree = parser.parse(code);
+
+const result = semantic.analyze(tree);
+
+console.log(JSON.stringify(result, null, 2));
---
+++ src/parser/semantic.types.ts
@@ -0,0 +1,21 @@
+export interface SemanticClass {
+  name: string;
+}
+
+export interface SemanticFunction {
+  name: string;
+}
+
+export interface SemanticImport {
+  path: string;
+}
+
+export interface SemanticResult {
+
+  classes: SemanticClass[];
+
+  functions: SemanticFunction[];
+
+  imports: SemanticImport[];
+
+}
